af7a65f20d48837e725f0289809b512bacdb67dd
xuanloi1999 <55650190+xuanloi1999@users.noreply.github.com>
2025-02-16 13:32:09 +0700

    completed project-7: questions

--- setup/app.js
+++ setup/app.js
@@ -1,2 +1,30 @@
 //using selectors inside the element
+// const questions = document.querySelectorAll('.question');
+
+// questions.forEach((question) => {
+//     const btn = question.querySelector('.question-btn');
+//     btn.addEventListener('click', function () {
+//         questions.forEach((item) => {
+//             if (item != question) {
+//                 item.classList.remove('show-text');
+//             }
+//         });
+//         question.classList.toggle('show-text');
+//     });
+// });
+
 // traversing the dom
+const questionButtons = document.querySelectorAll('.question-btn');
+questionButtons.forEach((questionButton) => {
+    questionButton.addEventListener('click', function () {
+        const parentEle = questionButton.parentElement.parentElement;
+
+        questionButtons.forEach((item) => {
+            const parentEleItem = item.parentElement.parentElement;
+            if (parentEleItem != parentEle) {
+                parentEleItem.classList.remove('show-text');
+            }
+        });
+        parentEle.classList.toggle('show-text');
+    });
+});
